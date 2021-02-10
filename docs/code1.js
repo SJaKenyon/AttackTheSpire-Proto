34cd631b689b8339fb35e9c71afe56f984cdaffa
SJaKenyon <b8018670@newcastle.ac.uk>
2021-02-10 14:53:08 +0000

    ProtoTypeGame

---
+++ docs/code1.js
@@ -0,0 +1,20 @@
+gdjs.TitleCode = {};
+
+gdjs.TitleCode.conditionTrue_0 = {val:false};
+gdjs.TitleCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.TitleCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.TitleCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.TitleCode.eventsList0(runtimeScene);
+return;
+
+}
+
+gdjs['TitleCode'] = gdjs.TitleCode;
